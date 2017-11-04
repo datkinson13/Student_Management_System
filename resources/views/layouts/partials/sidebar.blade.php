eeab73660f52e204c58bed7c2040b0855e66e2f5
hjone72 <hjone72@gmail.com>
2017-11-04 11:08:20 +1000

    Sidebar demonstration links
    These links are only here for demonstration purposes.

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,5 +1,10 @@
 <ul class="nav nav-pills flex-column">
     <li class="nav-item"><a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="/">Home</a></li>
     <li class="nav-item"><a class="nav-link {{ Request::is('course*') ? 'active' : '' }}" href="/course">Courses</a></li>
+</ul>
 
+
+<ul class="nav nav-pills flex-column">
+    <li class="nav-item">Demonstration purposes only:</li>
+    <li class="nav-item"><a class="nav-link {{ Request::is('login*') ? 'active' : '' }}" href="/login">Login</a></li>
 </ul>
