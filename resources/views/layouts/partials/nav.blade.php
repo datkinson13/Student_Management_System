2ee8edb7313423fdb51d8a7837531ce0ceb1a44e
hjone72 <hjone72@gmail.com>
2017-11-07 18:48:32 +1000

    Navigation
    * Added top nav bar links for login and register.
    * Ditch the "demo" section of the sidebar.
    * Fixed the top bar links to be "Active" correctly.

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -7,37 +7,42 @@
 
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
+            <li class="nav-item {{ Request::is('/') ? 'active' : '' }}">
                 <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
             </li>
         </ul>
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item active dropdown">
-                <!-- Will need to pass in $user object for name variables? -->
-                @if ($currentUser)
-                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button" aria-expanded="false"
-                           aria-haspopup="true">
-                            {{ $currentUser->Fname }} {{ $currentUser->Lname }} <span class="caret"></span>
-                        </a>
+            @if ($currentUser)
+                <li class="nav-item active dropdown">
+                    <!-- Will need to pass in $user object for name variables? -->
+                    <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button"
+                       aria-expanded="false"
+                       aria-haspopup="true">
+                        {{ $currentUser->Fname }} {{ $currentUser->Lname }} <span class="caret"></span>
+                    </a>
 
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
                                                                      document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
+                                Logout
+                            </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </li>
-                        </ul>
-                    </li>
-            @else
-                <a class="nav-link" href="{{ route('login') }}">Login</a>
-                @endif
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
+                    </ul>
                 </li>
+            @else
+                <li class="nav-item dropdown {{ Request::is('login*') ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>
+                </li>
+                <li class="nav-item dropdown {{ Request::is('register*') ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ route('register') }}">Register</a>
+                </li>
+            @endif
         </ul>
     </div>
 </nav>
