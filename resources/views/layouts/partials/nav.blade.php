f25f82c10c011acd1641a620c2ab58ef327c63db
hjone72 <hjone72@gmail.com>
2017-11-07 15:25:33 +1000

    Login
    Added a login page.
    
    * Added a login page that fits with our current theme.
    * After logining in the User's name appears in the top right of the page.
    * Added a logout button behind the users name. top right.
    * Added a view composer to ensure the 'currentUser' variable is avaliable in all blades that have a top nav bar `nav.blade.php` this will be expanded later to include more pages that may or may not require auth etc.
    
    See #3

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -11,11 +11,33 @@
                 <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
             </li>
         </ul>
-        <ul class = "navbar-nav ml-auto">
-          <li class = "nav-item active">
-            <!-- Will need to pass in $user object for name variables? -->
-            <a class = "nav-link" href = "#">John Smith</a>
-          </li>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active dropdown">
+                <!-- Will need to pass in $user object for name variables? -->
+                @if ($currentUser)
+                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button" aria-expanded="false"
+                           aria-haspopup="true">
+                            {{ $currentUser->Fname }} {{ $currentUser->Lname }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                                     document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+            @else
+                <a class="nav-link" href="{{ route('login') }}">Login</a>
+                @endif
+                </li>
         </ul>
     </div>
 </nav>
