388c20e662b59cf635c66b28f4b03e7948d09c2b
hjone72 <hjone72@gmail.com>
2017-11-12 17:40:35 +1000

    Create page
    Return the create view. Start work on create form.

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -35,7 +35,7 @@ class CourseController extends Controller
      */
     public function create()
     {
-        //
+        return view('course.create');
     }
 
     /**
---
+++ resources/views/course/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.master')
+
+@section('content')
+    <form method="POST" action="{{ route('course.create') }}">
+        <div class="jumbotron">
+            <a href="/course/create">
+                <button class="btn btn-primary user-profile-buttons">Save</button>
+            </a>
+            <div class="form-group">
+                <input type="text" class="form-control-lg" name="name" id="name" placeholder="Course Title">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="subtitle" id="subtitle" placeholder="subtitle">
+            </div>
+            <hr class="my-4">
+            <div class="form-group">
+                <textarea class="form-control" name="description" id="description" placeholder="Description...."></textarea>
+            </div>
+        </div>
+    </form>
+@endsection
