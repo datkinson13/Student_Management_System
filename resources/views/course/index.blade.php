77c2f46a892caf73ee1d884fc4f233a25570c5fa
hjone72 <hjone72@gmail.com>
2017-11-13 09:23:59 +1000

    Use and display the real data

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -4,6 +4,20 @@
     <a href = "/course/create"><button class = "btn btn-primary user-profile-buttons">Add Course</button></a>
     <h1>Courses</h1>
     <div class="card-deck">
+        @foreach ($courses as $course)
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <div class="card" style="width: 20rem;">
+                    <div class="card-body">
+                        <h4 class="card-title">{{ $course->name }}</h4>
+                        <p class="card-text">{{ $course->subtitle }}</p>
+                        <a href="/course/{{ $course->id }}" class="btn btn-primary">View Course</a>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated {{ $course->updated_at->diffForHumans() }}</small>
+                    </div>
+                </div>
+            </div>
+        @endforeach
         @for($i = 1; $i < 11; $i++)
             <div class="col-sm-6 col-md-4 col-lg-3">
                 <div class="card" style="width: 20rem;">
