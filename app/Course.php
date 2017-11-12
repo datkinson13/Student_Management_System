ffeccc84d7a9bbf82a222545b293aca6d9280aa5
hjone72 <hjone72@gmail.com>
2017-11-13 09:23:34 +1000

    declare fields as dates

--- app/Course.php
+++ app/Course.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     protected $fillable = ['name', 'subtitle', 'description', 'StartDate', 'EndDate', 'user_id', 'CourseTime'];
+
+    protected $dates = ['StartDate', 'EndDate'];
 }
