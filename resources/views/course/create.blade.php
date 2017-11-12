eb610ac35b785252419b8fe37292c2772a513fc9
hjone72 <hjone72@gmail.com>
2017-11-13 09:16:22 +1000

    Time picker
    Add a timepicker to the project. This allows for a nice humanreadable time to be selected. This can be jazzed up a bit more with css etc.
    
    https://jdewit.github.io/bootstrap-timepicker/

--- resources/views/course/create.blade.php
+++ resources/views/course/create.blade.php
@@ -14,6 +14,10 @@
                 <input type="text" class="form-control" name="subtitle" id="subtitle" placeholder="subtitle">
             </div>
             <hr class="my-4">
+            <div class="bootstrap-timepicker">
+                <input id="CourseTime" name="CourseTime" type="text" class="input-small">
+                <span class="oi" data-glyph="clock"></span>
+            </div>
             <div class="form-group">
                 <label for="StartDate">Start Date: </label>
                 <input type="date" class="form-control" name="StartDate" id="StartDate">
@@ -27,4 +31,17 @@
             </div>
         </div>
     </form>
+@endsection
+
+@section('page-script')
+    <script type="text/javascript" src="/js/bootstrap-timepicker.js"></script>
+    <script type="text/javascript">
+        jQuery(document).ready(function ($) {
+            $('#CourseTime').timepicker({
+                template: false,
+                showInputs: false,
+                minuteStep: 5
+            });
+        } );
+    </script>
 @endsection
