fa078986db22b52df05bf9c17d5071f0be91e249
hjone72 <hjone72@gmail.com>
2017-11-12 17:39:46 +1000

    Migration for course information
    Need to add information about the course to the DB.

---
+++ database/migrations/2017_11_11_024018_CourseDetails.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CourseDetails extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->string('name')->nullable();
+            $table->string('subtitle')->nullable();
+            $table->string('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
