c47bf23023feab7584352d053ae0a89c2280c88b
hjone72 <hjone72@gmail.com>
2017-11-13 18:18:15 +1000

    Override for prototype
    #21 - A small override to allow administrators full access until we build a system to add/remove permissions to a role.

--- app/Role.php
+++ app/Role.php
@@ -31,6 +31,10 @@ class Role extends Model {
 
     private function hasPermission($permission)
     {
+        // A small override to allow administrators full access until we build a system to add/remove permissions to a role.
+        if ($this->slug === 'administrator') {
+            return true;
+        }
         // if permission is set, return value, otherwise return false.
         if (array_key_exists($permission, $this->permissions)) {
             return $this->permissions[$permission] !== null ? $this->permissions[$permission] : false;
