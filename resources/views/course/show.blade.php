b210dfc477927c810cac8851e9a632e0f9ded67c
hjone72 <hjone72@gmail.com>
2017-11-14 18:49:52 +1000

    Add enrollment modal
    Enrollment modal popup. This will later be replaced with a form that allows the user to add documents etc etc. For now, for the prototype this should suffice.

--- resources/views/course/show.blade.php
+++ resources/views/course/show.blade.php
@@ -6,9 +6,40 @@
             <a class="btn btn-warning btn-lg" href="#" role="button">Edit Course</a>
             <a class="btn btn-success btn-lg" href="/course/{{ $course->id }}/email" role="button">Email Students</a>
         </h1>
-        <p class="lead">{{ $course->subtitle }}</p><a class="btn btn-primary btn-lg" href="#" role="button">Enroll</a>
+        <p class="lead">{{ $course->subtitle }}</p>
+        <a href="#">
+            <button type="button" class="btn btn-primary btn-lg user-action-buttons" data-toggle="modal"
+                    data-target="#course-enroll-modal-{{ $course->id }}">Enroll
+            </button>
+        </a>
         <p>{{ $course->StartDate->format('Y-m-d') }} - {{ $course->EndDate->format('Y-m-d') }} @ {{ $course->CourseTime }}</p>
         <hr class="my-4">
         <p>{{ $course->description }}</p>
+    </div>
+
+    <div class="modal fade" id="course-enroll-modal-{{ $course->id }}" tabindex="-1" role="dialog"
+         aria-labelledby="course-enroll-modal">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="myModalLabel">Enroll in: {{ $course->name }}?</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Are you sure you want to enroll in {{ $course->name }}?
+                </div>
+                <div class="modal-footer">
+                    <form method="POST" action="/course/{{ $course->id }}">
+                        {{ csrf_field() }}
+                        {{ method_field('POST') }}
+                        <button type="submit" class="btn btn-primary">Enroll</button>
+                    </form>
+
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
