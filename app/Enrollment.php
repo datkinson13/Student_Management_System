164d8ba6a4d0379c6e60e700e35310bb34cdc289
hjone72 <hjone72@gmail.com>
2017-11-17 14:46:53 +1000

    Allow fields to be filled

--- app/Enrollment.php
+++ app/Enrollment.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Enrollment extends Model
 {
-    protected $fillable = ['course_id', 'user_id'];
+    protected $fillable = ['course_id', 'user_id', 'ExpiryDate', 'CompletedDate'];
 }
