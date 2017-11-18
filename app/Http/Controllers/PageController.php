56d5f9a93b257bfbc933d0cfa04d9eadd343217d
hjone72 <hjone72@gmail.com>
2017-11-18 15:50:01 +1000

    Display on dashboard.

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,6 +8,7 @@ class PageController extends Controller
 {
     public function index()
     {
-      return view ('dashboard.index');
+        $enrollments = \Auth::user()->competencies;
+        return view ('dashboard.index', compact('enrollments'));
     }
 }
