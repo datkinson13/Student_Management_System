6906c0f3eae23a49cccfb397101ad9a4b7c3823a
hjone72 <hjone72@gmail.com>
2017-11-19 14:00:06 +1000

    Employee to Employer
    Define relationship between employee and employer

--- app/Employee.php
+++ app/Employee.php
@@ -9,4 +9,8 @@ class Employee extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    public function employer() {
+        return $this->belongsTo(Employer::class);
+    }
 }
