fbf1e975b4c1fc11eb4cc23610c606b9b349819d
hjone72 <hjone72@gmail.com>
2017-11-19 13:12:58 +1000

    Update course seeder
    Update the course seeder until we correct the storage size of the description.

--- database/seeds/CourseSeeder.php
+++ database/seeds/CourseSeeder.php
@@ -12,7 +12,7 @@ class CourseSeeder extends Seeder
      */
     public function run()
     {
-        Course::create([
+/*        Course::create([
             'name' => 'First Aid',
             'subtitle' => 'Learn to identify and eliminate potentially hazardous conditions in their environment, recognize emergencies and make appropriate decisions for first aid care.',
             'description' => 'The purpose of this course is to help participants identify and eliminate potentially hazardous conditions in their environment, recognize emergencies and make appropriate decisions for first aid care. It teaches skills that participants need to know in order to provide immediate care for a suddenly ill or injured person until more advanced medical care arrives to take over.',
@@ -36,6 +36,34 @@ class CourseSeeder extends Seeder
             'name' => 'First Aid v3',
             'subtitle' => 'Learn to identify and eliminate potentially hazardous conditions in their environment, recognize emergencies and make appropriate decisions for first aid care.',
             'description' => 'The purpose of this course is to help participants identify and eliminate potentially hazardous conditions in their environment, recognize emergencies and make appropriate decisions for first aid care. It teaches skills that participants need to know in order to provide immediate care for a suddenly ill or injured person until more advanced medical care arrives to take over.',
+            'StartDate' => '2017-11-15',
+            'EndDate' => '2017-11-20',
+            'CourseTime' => '10:00:00',
+            'user_id' => 1
+        ]);*/
+
+        Course::create([
+            'name' => 'First Aid',
+            'subtitle' => 'Learn to identify and eliminate potentially hazardous conditions in their environment, recognize emergencies and make appropriate decisions for first aid care.',
+            'description' => 'test',
+            'StartDate' => '2017-11-15',
+            'EndDate' => '2017-11-20',
+            'CourseTime' => '10:00:00',
+            'user_id' => 1
+        ]);
+        Course::create([
+            'name' => 'First Aid v2',
+            'subtitle' => 'Learn to identify and eliminate potentially hazardous conditions in their environment, recognize emergencies and make appropriate decisions for first aid care.',
+            'description' => 'test',
+            'StartDate' => '2017-11-15',
+            'EndDate' => '2017-11-20',
+            'CourseTime' => '10:00:00',
+            'user_id' => 1
+        ]);
+        Course::create([
+            'name' => 'First Aid v3',
+            'subtitle' => 'Learn to identify and eliminate potentially hazardous conditions in their environment, recognize emergencies and make appropriate decisions for first aid care.',
+            'description' => 'test',
             'StartDate' => '2017-11-15',
             'EndDate' => '2017-11-20',
             'CourseTime' => '10:00:00',
