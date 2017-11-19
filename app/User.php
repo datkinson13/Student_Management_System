bede51144855ff166fa8b238e4211464b9f75c1c
hjone72 <hjone72@gmail.com>
2017-11-19 14:00:23 +1000

    User to employee
    Define user to employee

--- app/User.php
+++ app/User.php
@@ -54,6 +54,10 @@ class User extends Authenticatable
         return $this->hasMany(Enrollment::class);
     }
 
+    public function employee() {
+        return $this->hasOne(Employee::class);
+    }
+
     public function employer() {
         return $this->hasOne(Employer::class);
     }
