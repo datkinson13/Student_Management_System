a7a9ec4f548c575f253ed0696d99e28189e42e52
hjone72 <hjone72@gmail.com>
2017-11-21 21:34:57 +1000

    Dashboard updates
    - Display "Employer" dashboard for employers.
    - Display employee names for employer
    - Don't display dashboard if user isn't logged in. This needs cleaning up.

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,48 +3,65 @@
 @section('content')
     <h1>Dashboard</h1>
 
-    <section class="row text-center placeholders">
-        @foreach ($enrollments as $enrollment)
-            <div class="col-6 col-sm-3 placeholder">
-                <img src="data:image/gif;base64,{{ $enrollment->competencyStatus()['light'] }}" width="200" height="200"
-                     class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
-                <h4>{{ $enrollment->course->name }}</h4>
-                <div class="text-muted">{{ $enrollment->ExpiryDate }}</div>
-            </div>
-            @if ($loop->iteration == 4)
-                {{-- This is the 4th loop, break now. We are only showing the 4 closest to expiry. --}}
-                @break
-            @endif
-        @endforeach
-    </section>
-
-    <h2>My Competencies</h2>
-    <div class="table-responsive">
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Course</th>
-                <th>Date Completed</th>
-                <th>Status</th>
-                <th>Expiry Date</th>
-                <th>Days remaining</th>
-                <th>Calculated level</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($enrollments as $enrollment)
-                <tr>
-                    <td>{{ $enrollment->id }}</td>
-                    <td>{{ $enrollment->course->name }}</td>
-                    <td>{{ $enrollment->CompletedDate }}</td>
-                    <td>{{ $enrollment->enrolment_status }}</td>
-                    <td>{{ $enrollment->ExpiryDate }}</td>
-                    <td>{{ $enrollment->daysRemaining() }}</td>
-                    <td>{{ $enrollment->competencyStatus()['color'] }}</td>
-                </tr>
+    @if ($currentUser)
+        <section class="row text-center placeholders">
+            @foreach ($enrollments as $enrollment)
+                <div class="col-6 col-sm-3 placeholder">
+                    <img src="data:image/gif;base64,{{ $enrollment->competencyStatus()['light'] }}" width="200" height="200"
+                         class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
+                    <h4>{{ $enrollment->course->name }}</h4>
+                    <div class="text-muted">
+                        {{ $enrollment->ExpiryDate }}
+                        @if ($currentUser->isEmployer())
+                            - {{ $enrollment->user->Fname }} {{ $enrollment->user->Lname }}
+                        @endif
+                    </div>
+                </div>
+                @if ($loop->iteration == 4)
+                    {{-- This is the 4th loop, break now. We are only showing the 4 closest to expiry. --}}
+                    @break
+                @endif
             @endforeach
-            </tbody>
-        </table>
-    </div>
+        </section>
+
+        @if ($currentUser->isEmployer())
+            <h2>Employee Competencies</h2>
+        @else
+            <h2>My Competencies</h2>
+        @endif
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    @if ($currentUser->isEmployer())
+                        <th>Employee</th>
+                    @endif
+                    <th>Course</th>
+                    <th>Date Completed</th>
+                    <th>Status</th>
+                    <th>Expiry Date</th>
+                    <th>Days remaining</th>
+                    <th>Calculated level</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($enrollments as $enrollment)
+                    <tr>
+                        @if ($currentUser->isEmployer())
+                            <td>{{ $enrollment->user->Fname }} {{ $enrollment->user->Lname }}</td>
+                        @endif
+                        <td>{{ $enrollment->course->name }}</td>
+                        <td>{{ $enrollment->CompletedDate }}</td>
+                        <td>{{ $enrollment->enrolment_status }}</td>
+                        <td>{{ $enrollment->ExpiryDate }}</td>
+                        <td>{{ $enrollment->daysRemaining() }}</td>
+                        <td>{{ $enrollment->competencyStatus()['color'] }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    @else
+        <p>To gain full access to the dashboard please login.</p>
+    @endif
 @endsection
