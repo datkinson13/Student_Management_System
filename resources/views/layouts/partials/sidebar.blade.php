b1abb288c89c62739321be66cb85290f0f244abf
hjone72 <hjone72@gmail.com>
2018-01-05 17:02:26 +1000

    hide items for unauthed users
    Hide items for users who aren't logged in.

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,13 +1,13 @@
 <ul class="nav nav-pills flex-column">
     <li class="nav-item"><a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="/">Home</a></li>
-    <li class="nav-item"><a class="nav-link {{ Request::is('users*') ? 'active' : '' }}" href="/users">Users</a></li>
+    @if (Auth::check())<li class="nav-item"><a class="nav-link {{ Request::is('users*') ? 'active' : '' }}" href="/users">Users</a></li>@endif
     <li class="nav-item"><a class="nav-link {{ Request::is('course*') ? 'active' : '' }}" href="/course">Courses</a></li>
-    <li class="nav-item"><a class="nav-link {{ Request::is('businessroles*') ? 'active' : '' }}" href="/businessroles">Business Roles</a></li>
-    <li class="nav-item"><a class="nav-link {{ Request::is('/competencies/monitor') ? 'active' : '' }}" href="/competencies/monitor">Competency Monitor</a></li>
-    <li class="nav-item"><a class="nav-link {{ Request::is('/trainingliability/calculate') ? 'active' : '' }}" href="/trainingliability/calculate">Net Training Liability</a></li>
+    @if (Auth::check())<li class="nav-item"><a class="nav-link {{ Request::is('businessroles*') ? 'active' : '' }}" href="/businessroles">Business Roles</a></li>@endif
+    @if (Auth::check())<li class="nav-item"><a class="nav-link {{ Request::is('/competencies/monitor') ? 'active' : '' }}" href="/competencies/monitor">Competency Monitor</a></li>@endif
+    @if (Auth::check())<li class="nav-item"><a class="nav-link {{ Request::is('/trainingliability/calculate') ? 'active' : '' }}" href="/trainingliability/calculate">Net Training Liability</a></li>@endif
     <li class="nav-item"><a class="nav-link {{ Request::is('/calendar*') ? 'active' : '' }}" href="/calendar">Calendar</a></li>
-    <li class="nav-item"><a class="nav-link {{ Request::is('reports*') ? 'active' : '' }}" href="/reports">Reports</a></li>
-    <li class="nav-item"><a class="nav-link {{ Request::is('enrollment*') ? 'active' : '' }}" href="/enrollment">Enrollment</a></li>
+    @if (Auth::check())<li class="nav-item"><a class="nav-link {{ Request::is('reports*') ? 'active' : '' }}" href="/reports">Reports</a></li>@endif
+    @if (Auth::check())<li class="nav-item"><a class="nav-link {{ Request::is('enrollment*') ? 'active' : '' }}" href="/enrollment">Enrollment</a></li>@endif
     <hr/>
     <li class="nav-item"><a class="nav-link {{ Request::is('tickets*') ? 'active' : '' }}" href="/tickets">Help</a></li>
 </ul>
