1e8559f9a9a7a979310d837101dd0a7c41b7e3f7
DAtkinson13 <dylan.atkinson13@gmail.com>
2018-01-20 16:56:10 +1100

    Fix course display page

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -5,8 +5,8 @@
     <h1>Courses</h1>
     <div class="card-deck">
         @foreach ($courses as $course)
-            <div class="col-sm-6 col-md-4 col-lg-3">
-                <div class="card" style="width: 20rem;">
+            <div class="col-sm-6 col-md-4 col-lg-4">
+                <div class="card" style="width: 20rem; height: 380px; margin: 10px;">
                     <div class="card-body">
                         <h4 class="card-title">{{ $course->name }}</h4>
                         <p class="card-text">{{ $course->subtitle }}</p>
